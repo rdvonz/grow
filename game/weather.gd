285fca013afef64cd170cbeee8786dd5019684bc
tapdancingmetroid <tapdancingmetroid@gmail.com>
2015-12-12 14:03:35 -0800

    swaps audio tracks at correct times now, but plays start of track briefly before actually jumping to proper time in music.

--- game/weather.gd
+++ game/weather.gd
@@ -15,39 +15,46 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("one"):
+		if weather != 1:
+			get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
+			print("HEA")
+			musicTime = currentMusic.get_pos()
+			currentMusic.stop()
+			currentMusic = get_node("Music1")
+			currentMusic.play()
+			currentMusic.seek_pos(musicTime)
 		weather = 1
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
-		print("HEA")
-		musicTime = currentMusic.get_pos()
-		currentMusic.stop()#pressng 1 should stop music now, but that's not happening
-		#currentMusic = get_node("Music1")
-		#currentMusic.set_pos(musicTime)
-		#currentMusic.play()
 	if event.is_action_pressed("two"):
+		if weather != 2:
+			get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
+			musicTime = currentMusic.get_pos()
+			stop_music()
+			currentMusic = get_node("Music2")
+			currentMusic.play()
+			currentMusic.seek_pos(musicTime)
 		weather = 2
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
-		musicTime = currentMusic.get_pos()
-		stop_music()
 	if event.is_action_pressed("three"):
+		if weather != 3:
+			get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
+			musicTime = currentMusic.get_pos()
+			stop_music()
+			currentMusic = get_node("Music3")
+			currentMusic.play()
+			currentMusic.seek_pos(musicTime)
 		weather = 3
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
-		musicTime = currentMusic.get_pos()
-		currentMusic.stop()
-		currentMusic = get_node("Music3")
-		currentMusic.set_pos(musicTime)
-		currentMusic.play()
 	if event.is_action_pressed("four"):
+		if weather != 4:
+			get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
+			musicTime = currentMusic.get_pos()
+			stop_music()
+			currentMusic = get_node("Music4")
+			currentMusic.play()
+			currentMusic.seek_pos(musicTime)
 		weather = 4
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
-		musicTime = currentMusic.get_pos()
-		currentMusic.stop()
-		currentMusic = get_node("Music4")
-		currentMusic.set_pos(musicTime)
-		currentMusic.play()
 		
 func stop_music():
 	currentMusic.stop()
-	print("hey, shoulda stopped")
+	#print("hey, shoulda stopped")
 	
 
 func _process(delta):
