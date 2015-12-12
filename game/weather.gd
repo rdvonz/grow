42b8ba18dba47443a8e015e8ea86812d10bff763
Paul Von Zimmerman <pcvonz@gmail.com>
2015-12-11 23:17:38 -0800

    updated controls to is_pressed

--- game/weather.gd
+++ game/weather.gd
@@ -7,25 +7,36 @@ var weather = 1
 
 func _ready():
 	set_process(true)
-	set_process_input(true)
 
 func _input(event):
-	if Input.is_action_pressed("one"):
+	if is_pressed("one"):
 		weather = 1
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
-	if Input.is_action_pressed("two"):
+		print("HEA")
+	if is_pressed("two"):
 		weather = 2
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
-	if Input.is_action_pressed("three"):
+	if is_pressed("three"):
 		weather = 3
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
-	if Input.is_action_pressed("four"):
+	elif is_pressed("four"):
 		weather = 4
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
 
 
 func _process(delta):
-
+	if Input.is_action_pressed("one"):
+		weather = 1
+		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
+	elif Input.is_action_pressed("two"):
+		weather = 2
+		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
+	elif Input.is_action_pressed("three"):
+		weather = 3
+		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
+	elif Input.is_action_pressed("four"):
+		weather = 4
+		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
 	if weather == 1:
 		pass
 	elif weather == 2:
