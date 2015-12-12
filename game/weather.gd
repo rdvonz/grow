6392b44b2de13251042e582496335b78f14acb07
tapdancingmetroid <tapdancingmetroid@gmail.com>
2015-12-12 12:52:08 -0800

    added music happenings maybe?

--- game/weather.gd
+++ game/weather.gd
@@ -4,6 +4,9 @@ extends Node2D
 # member variables here, example:
 # var a=2
 var weather = 1
+var currentMusic = get_node("Music1")
+var musicTime = 0.0
+
 
 func _ready():
 	set_process(true)
@@ -13,15 +16,35 @@ func _input(event):
 		weather = 1
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
 		print("HEA")
+		musicTime = currentMusic.get_pos()
+		currentMusic.stop()
+		currentMusic = get_node("Music1")
+		currentMusic.set_pos(musicTime)
+		currentMusic.play()
 	if is_pressed("two"):
 		weather = 2
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
+		musicTime = currentMusic.get_pos()
+		currentMusic.stop()
+		currentMusic = get_node("Music2")
+		currentMusic.set_pos(musicTime)
+		currentMusic.play()
 	if is_pressed("three"):
 		weather = 3
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
+		musicTime = currentMusic.get_pos()
+		currentMusic.stop()
+		currentMusic = get_node("Music3")
+		currentMusic.set_pos(musicTime)
+		currentMusic.play()
 	elif is_pressed("four"):
 		weather = 4
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
+		musicTime = currentMusic.get_pos()
+		currentMusic.stop()
+		currentMusic = get_node("Music4")
+		currentMusic.set_pos(musicTime)
+		currentMusic.play()
 
 
 func _process(delta):
