ba9fe2ab5cac0bf6e0c5798b9698424bc38fb963
Paul Von Zimmerman <pcvonz@gmail.com>
2015-12-12 13:53:43 -0800

    set_process_input to true in weather)

--- game/weather.gd
+++ game/weather.gd
@@ -9,12 +9,12 @@ var musicTime = 0.0
 
 
 func _ready():
-	set_process(true)
+	set_process_input(true)
 	currentMusic = get_node("Music1")
 	currentMusic.play()
 
 func _input(event):
-	if is_pressed("one"):
+	if event.is_action_pressed("one"):
 		weather = 1
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
 		print("HEA")
@@ -23,15 +23,12 @@ func _input(event):
 		#currentMusic = get_node("Music1")
 		#currentMusic.set_pos(musicTime)
 		#currentMusic.play()
-	if is_pressed("two"):
+	if event.is_action_pressed("two"):
 		weather = 2
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
 		musicTime = currentMusic.get_pos()
-		currentMusic.stop()
-		currentMusic = get_node("Music2")
-		currentMusic.set_pos(musicTime)
-		currentMusic.play()
-	if is_pressed("three"):
+		stop_music()
+	if event.is_action_pressed("three"):
 		weather = 3
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
 		musicTime = currentMusic.get_pos()
@@ -39,7 +36,7 @@ func _input(event):
 		currentMusic = get_node("Music3")
 		currentMusic.set_pos(musicTime)
 		currentMusic.play()
-	elif is_pressed("four"):
+	if event.is_action_pressed("four"):
 		weather = 4
 		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
 		musicTime = currentMusic.get_pos()
@@ -47,21 +44,14 @@ func _input(event):
 		currentMusic = get_node("Music4")
 		currentMusic.set_pos(musicTime)
 		currentMusic.play()
-
+		
+func stop_music():
+	currentMusic.stop()
+	print("hey, shoulda stopped")
+	
 
 func _process(delta):
-	if Input.is_action_pressed("one"):
-		weather = 1
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.2, get_node("Bleh").get_pos().y))
-	elif Input.is_action_pressed("two"):
-		weather = 2
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.4, get_node("Bleh").get_pos().y))
-	elif Input.is_action_pressed("three"):
-		weather = 3
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.6, get_node("Bleh").get_pos().y))
-	elif Input.is_action_pressed("four"):
-		weather = 4
-		get_node("Bleh").set_pos(Vector2(get_viewport_rect().size.width*0.8, get_node("Bleh").get_pos().y))
+	
 	if weather == 1:
 		pass
 	elif weather == 2:
